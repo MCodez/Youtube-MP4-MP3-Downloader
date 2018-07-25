5018377cad8c088d17403a3e7c582cf66c40a05a
LALIT ARORA <lalitarora1997@gmail.com>
2018-07-25 22:00:16 +0530

    downloader script

---
+++ downloader.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 25 19:44:44 2018
+
+@author: LALIT ARORA
+"""
+
+from __future__ import unicode_literals
+import youtube_dl
+import urllib.parse
+from bs4 import BeautifulSoup
+
+def youtube(searchterm):
+    query=urllib.parse.quote(searchterm)
+    url="https://www.youtube.com/results?search_query="+query
+    response=urllib.request.urlopen(url)
+    html=response.read()
+    soup=BeautifulSoup(html,"lxml")
+    links=[]
+    names=[]
+    for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
+        links.append('https://www.youtube.com' + vid['href'])
+        try:
+            names.append(vid['title'])
+        except:
+            names.append("")
+                
+    return (links[:5],names[:5])
+
+def download_mp3(url):
+    ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+            }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    
+def download_mp4(url):
+    ydl_opts = {}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+def search(term):
+    res=youtube(term)
+    for i in range(5):
+        print(str(i+1)+" "+str(res[1][i]))
+    return res
+    
+if __name__ == "__main__":
+    import sys
+    arguments=sys.argv
+    searchterm=arguments[1]
+    g=search(searchterm)
+    print("\nEnter the number of song or video to be downloaded in format <1 video> or <1 music>")
+    mode=input()
+    temp=mode.split(' ')
+    number=int(temp[0])
+    m=temp[1]
+    if (m=="video"):
+        download_mp4(g[0][number-1])
+    elif (m=="music"):
+        download_mp3(g[0][number-1])
+    else:
+        print("Wrong number entered.")
+        exit(0)
+        
